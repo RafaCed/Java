517cb71cb2b8f40f33c6dff5b5606228d31179da
RafaCed <cederbergrafael@gmail.com>
2024-09-03 11:13:49 +0300

    uppgift6

---
+++ uppgift6.java
@@ -0,0 +1,27 @@
+
+/**
+ *
+ * @author ceder
+ */
+public class uppgift6 {
+
+    
+    public static void main(String args[]) {
+        
+        
+        
+       boolean lightOn = true;
+       boolean lightOff = false;
+       
+       int lightSwitchOn = 1;
+       int lightSwitchOff = 0;
+       if(lightSwitchOn ==1) {
+           System.out.println("Light On");
+       }else{
+           System.out.println("Light Off");
+       }
+       
+       
+       
+    }
+}
